f04286929a3d19b4af791e7c3c387f64931d0af6
yapcheahshen <yapcheahshen@gmail.com>
2015-01-09 17:51:57 +0800

    fix listapp

--- ksana.js
+++ ksana.js
@@ -1,6 +1,6 @@
 jsonp_handler({
  "version": "1",
- "build": 2,
+ "build": 7,
  "title": "moedict-yu",
  "minruntime": 4,
  "baseurl": "http://rawgit.com/ahsyuyu/moedict-yu/master/",
@@ -10,6 +10,8 @@ jsonp_handler({
   "bundle.js",
   "main.css",
   "jquery.js",
+  "bootstrap.min.css",
+  "bootstrap.js.css",
   "react-with-addons.js",
   "ksana.js",
   "http://ya.ksana.tw/kdb/moedict.kdb"
@@ -20,17 +22,17 @@ jsonp_handler({
   117,
   277980,
   644464,
-  306,
+  669,
   50141498
  ],
  "filedates": [
   "2015-01-09T09:15:18.000Z",
-  "2015-01-09T09:18:18.000Z",
+  "2015-01-09T09:49:12.000Z",
   "2015-01-09T09:15:18.000Z",
   "2015-01-09T09:15:18.000Z",
   "2015-01-09T09:15:18.000Z",
-  "2015-01-09T09:15:18.000Z",
+  "2015-01-09T09:48:38.000Z",
   "2015-01-09T09:15:18.000Z"
  ],
- "date": "2015-01-09T09:18:19.303Z"
+ "date": "2015-01-09T09:49:12.468Z"
 })
