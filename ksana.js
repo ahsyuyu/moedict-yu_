9653d53799406e47317774c5e60a7784e13a908c
yapcheahshen <yapcheahshen@gmail.com>
2015-01-09 17:19:15 +0800

    fix ksana.js

--- ksana.js
+++ ksana.js
@@ -1,11 +1,36 @@
 jsonp_handler({
-"version": "1",
-"build": 1,
-"title":"moedict-yu",
-"minruntime": 4,
-"baseurl":"http://rawgit.com/ahsyuyu/moedict-yu/master/",
-"description":"",
-"files":
-  ["index.html","bundle.js","main.css","jquery.js","react-with-addons.js","ksana.js",
-  "http://ya.ksana.tw/kdb/moedict.kdb"]
+ "version": "1",
+ "build": 2,
+ "title": "moedict-yu",
+ "minruntime": 4,
+ "baseurl": "http://rawgit.com/ahsyuyu/moedict-yu/master/",
+ "description": "",
+ "files": [
+  "index.html",
+  "bundle.js",
+  "main.css",
+  "jquery.js",
+  "react-with-addons.js",
+  "ksana.js",
+  "http://ya.ksana.tw/kdb/moedict.kdb"
+ ],
+ "filesizes": [
+  371,
+  444,
+  117,
+  277980,
+  644464,
+  306,
+  50141498
+ ],
+ "filedates": [
+  "2015-01-09T09:15:18.000Z",
+  "2015-01-09T09:18:18.000Z",
+  "2015-01-09T09:15:18.000Z",
+  "2015-01-09T09:15:18.000Z",
+  "2015-01-09T09:15:18.000Z",
+  "2015-01-09T09:15:18.000Z",
+  "2015-01-09T09:15:18.000Z"
+ ],
+ "date": "2015-01-09T09:18:19.303Z"
 })
